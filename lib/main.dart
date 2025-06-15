aea10248ca735aa49255ae38b5dd35093bd41dd0
Aayush <aayushghimire2001@gmail.com>
2025-06-15 19:01:11 +0545

    form validation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:new_app/home.dart';
+import 'package:new_app/register.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Homepage()
+      home: RegisterformUI()
     );
   }
 }
